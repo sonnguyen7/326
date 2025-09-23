6e0374f36fa19798950d5cf77c4e6771b9cc889e
Son H. Nguyen <chungwizas@gmail.com>
2025-09-23 00:37:15 -0700

    Create threads.c

---
+++ threads.c
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+
+static void *child(void *ignored){
+while (1){ // this function allows infinite loops
+   sleep(3);
+   printf("Child is done sleeping 3 seconds.\n");
+   return NULL;
+   }
+}
+
+int main(int argc, char *argv[]){
+   pthread_t child_thread;
+   int code;
+   code = pthread_create(&child_thread, NULL, child, NULL);
+   if(code){
+      fprintf(stderr, "pthread_create failed with code %d\n", code);
+   }
+   int input = getchar();
+   if(input == "\n"){
+       pthread_cancel(child_thread);
+   }
+
+   sleep(5);
+   printf("Parent is done sleeping 5 seconds.\n");
+   return 0;
+}
