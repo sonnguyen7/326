5c5c4a0c2bf3200cd1ac7ac45e87b15e83609ea5
Son H. Nguyen <chungwizas@gmail.com>
2025-09-23 01:58:37 -0700

    Update threads.c

--- threads.c
+++ threads.c
@@ -1,31 +1,31 @@
-#include <pthread.h>
-#include <unistd.h>
-#include <stdio.h>
+#include <pthread.h> // pthread library
+#include <unistd.h> // for sleep()
+#include <stdio.h> // for printf()
 
-static void *child(void *ignored){
+static void *child(void *ignored){ // the function for the separate child thread
 while (1){ // this function allows infinite loops
-   sleep(3);
-   printf("Child thread is done sleeping 3 seconds.\n");
+   sleep(3); // sleep for 3 seconds
+   printf("Child thread is done sleeping 3 seconds.\n"); // print this message after the 3 second pause
    }
-   return NULL;
+   return NULL; // to avoid compiler warnings
 }
 
 int main(int argc, char *argv[]){
-   printf("Press Enter to cancel the child thread.\n");
-   pthread_t child_thread;
+   printf("Press Enter to cancel the child thread.\n"); // notify the user to press enter if they want to terminate the child thread
+   pthread_t child_thread; // create a pthread variable
    int code;
    code = pthread_create(&child_thread, NULL, child, NULL);
    if(code){
       fprintf(stderr, "pthread_create failed with code %d\n", code);
    }
-   int input = getchar();
-   if(input == '\n'){
-       pthread_cancel(child_thread);
-       printf("Child thread is now abandoned.\n");
+   int input = getchar(); // get user input from keyboard(enter key)
+   if(input == '\n'){ // if the user pressed enter key(newline character), kill the child thread
+       pthread_cancel(child_thread); 
+       printf("Child thread is now abandoned.\n"); // notify the user that the child thread has been terminated
    }
 
-   sleep(5);
-   printf("Parent thread is done sleeping 5 seconds.\n");
-   printf("Program ended.\n");
+   sleep(5); // sleep for 5 seconds
+   printf("Parent thread is done sleeping 5 seconds.\n"); // print this message after the 5 second pause
+   printf("Ending program.\n"); // notify the user that the program is ending
    return 0;
 }
